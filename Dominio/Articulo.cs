2f025a75f79ec2454f5af0db8c72c4bb07b82509
Joaquín Alduncin <joacoaldun@gmail.com>
2023-04-26 16:53:50 -0300

    Primera subida, Joaquin Alduncin.

---
+++ Dominio/Articulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Articulo
+    {
+        public int CodigoArticulo  { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public float Precio { get; set; }
+        public Marca Marcas { get; set; }
+
+        public Categoria Categorias { get; set; }
+
+        public List<Imagen> Imagenes { get; set;}
+
+    }
+}
---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+
+
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+
+            conexion = new SqlConnection("server=.\\SQLLaboratorio; database=POKEDEX_DB; integrated security=true;");
+            comando = new SqlCommand();
+
+        }
+
+        public void setearConsulta(string consulta)
+        {
+
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarConsulta()
+        {
+            comando.Connection = conexion;
+
+            try
+            {
+                conexion.Open();
+                //EJECUTO LA LECTURA
+                lector = comando.ExecuteReader();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+
+        }
+
+        public void cerrarConexion()
+        {
+
+            if (lector != null)
+                lector.Close();
+
+            conexion.Close();
+
+
+        }
+
+
+
+    }
+}
